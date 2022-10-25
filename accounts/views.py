eb0a2445caf7a58b46e2793ce6dee055c2b68923
David Funnell <dfunnell77@gmail.com>
2022-10-24 17:43:06 -0700

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate,logout
-from accounts.forms import LoginForm
+from django.contrib.auth import login, authenticate, logout
+from accounts.forms import LoginForm, SignupForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -11,12 +12,7 @@ def user_login(request):
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
-
-            user = authenticate(
-                request,
-                username=username,
-                password=password,
-            )
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect("list_projects")
@@ -31,3 +27,24 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+            if password == password_confirmation:
+                user = User.objects.create_user(username, password=password)
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignupForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "registration/signup.html", context)
